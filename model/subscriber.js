56e509415f5b39587cddc1369862f1bd4108e11c
Tarun Sharma <starun.1998@gmail.com>
2021-03-13 23:35:27 +0530

    db and route setup for user

---
+++ model/subscriber.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose")
+
+var Subscriber = new mongoose.Schema({
+	name : {
+		type : String ,
+		required : true
+	},
+	batch : {
+		type : Number  ,
+		required : true
+	},
+	email : {
+		type : String ,
+		required : true,
+		unique : true
+	}
+})
+
+module.export = mongoose.model('Subscribers', Subscriber) ;
